7855dafbf42826361cab6172a125babaf2354583
Lewis Phillips <lewis@wearecampfire.co.uk>
2014-11-05 14:09:54 +0000

    Install Devise and create users with First Name, Last Name, User Name, email, password, DOB, location and gender. Restyle and permission Sign Up form to allow user creation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,36 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+      before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
+            :first_name, 
+            :last_name, 
+            :user_name, 
+            :email, 
+            :encrypted_password,
+            :password, 
+            :current_password,
+            :date_of_birth,
+            :location,
+            :its_female, ) }
+
+ devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+            :first_name, 
+            :last_name, 
+            :user_name, 
+            :email, 
+            :encrypted_password,
+            :password, 
+            :current_password,
+            :date_of_birth,
+            :location,
+            :its_female, ) }
+
+
+        end
 end
