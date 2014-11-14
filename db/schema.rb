b3e866236d98445454a19e6254d3eb15bd6f7641
Lewis Phillips <lewis@wearecampfire.co.uk>
2014-11-14 00:45:23 +0000

    Create Posts controller and assign to users. Destroy link pointing to destroyed post. Created posts are from uncomplete user accounts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111124037) do
+ActiveRecord::Schema.define(version: 20141114002833) do
 
   create_table "articles", force: true do |t|
     t.text     "name"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 20141111124037) do
     t.datetime "updated_at"
     t.string   "ext_url"
   end
+
+  create_table "posts", force: true do |t|
+    t.text     "content"
+    t.string   "img_url"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
