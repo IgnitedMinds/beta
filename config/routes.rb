0bad5ae45224045740689274155da4e6a699a2f2
Lewis Phillips <lewis@wearecampfire.co.uk>
2014-11-24 00:57:55 +0000

    Add alternate routes for authenticated and non-authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
   
-  root to: 'landing#index'
+  authenticated :user do
+  root :to => 'articles#index', as: :authenticated_root
+end
+
+root to: 'landing#index'
   
   get '/:id', to: 'profiles#show'
 
