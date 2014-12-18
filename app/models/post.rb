9ae75dd6b9940bb62ca80a7334aab65c342e1e3e
Lewis Phillips <lewis@wearecampfire.co.uk>
2014-12-18 17:43:23 +0000

    Apply default order to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
 	has_attached_file :postimg
 	validates_attachment_content_type :postimg, :content_type => /\Aimage\/.*\Z/
-
+	default_scope { order('created_at DESC') }
 belongs_to :user
 has_many :comments
 
