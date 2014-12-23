9ee286a28f000c115937fdfca1fa1701953cd966
Lewis Phillips <lewis@wearecampfire.co.uk>
2014-12-23 21:39:26 +0000

    Add create method back to Events and Funds. Fix CSS

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,3 +1,69 @@
 class FundsController < InheritedResources::Base
-  actions :index
+  actions :index, :show, :edit, :update, :destroy
+
+
+
+  def index
+    @funds = Fund.all
+  end
+
+  def show
+  end
+
+  def new
+    @fund = Fund.new
+  end
+
+  def edit
+  end
+
+  def create
+    @fund = Fund.new(fund_params)
+
+    respond_to do |format|
+      if @fund.save
+        format.html { redirect_to action: "index" }
+        format.json { render :index }
+      else
+        format.html { render :new }
+        format.json { render json: @fund.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @fund.update(fund_params)
+        format.html { redirect_to @fund }
+        format.json { render :show, status: :ok, location: @fund }
+      else
+        format.html { render :edit }
+        format.json { render json: @fund.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @fund.destroy
+    respond_to do |format|
+      format.html { redirect_to funds_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_fund
+    @fund = Fund.find(params[:id])
+  end
+
+  def fund_params
+    params.require(:fund).permit(
+    :title,
+    :source,
+    :type,
+    :age_range,
+    :deadline,
+    :location,
+    :ext_url,)
+  end
 end
