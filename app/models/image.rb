7c6d3181d6e4fdb109159eee6e1e90720f432be7
Lewis Phillips <lewis@wearecampfire.co.uk>
2015-01-09 16:33:48 +0000

    What is image.rb? Where did it come from?

--- app/models/image.rb
+++
@@ -1,6 +0,0 @@
-class Article < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "512x512>", :thumb => "128x128>" }, :default_url => "/articles/:style/missing.png"
-  validates_attachment :avatar, :presence => true,
-                                :content_type => { :content_type => ["image/jpg", "image/gif", "image/png"] },
-                                :size => { :in => 0..512.kilobytes }
-end
