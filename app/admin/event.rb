51859c21201b9d3a494442794ee6a0dac8b6d7c6
Lewis Phillips <lewis@wearecampfire.co.uk>
2015-01-09 16:51:37 +0000

    Make date fields more sensible

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -15,4 +15,16 @@ ActiveAdmin.register Event do
   # end
   permit_params :title, :location, :venue, :date_time, :description, :ext_url
 
+  form do |f|
+    f.inputs "Event" do
+      f.input :title
+      f.input :location
+      f.input :venue
+      f.input :date_time, start_year: Time.now.year - 2, end_year: Time.now.year + 5
+      f.input :description
+      f.input :ext_url
+    end
+    f.actions
+  end
+
 end
