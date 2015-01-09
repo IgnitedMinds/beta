57c7e1e056cdc7a84ba839d18f4255203690bc76
Lewis Phillips <lewis@wearecampfire.co.uk>
2015-01-09 16:46:48 +0000

    Make a tag select field for articles

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -13,12 +13,13 @@ ActiveAdmin.register Article do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-  permit_params :user_id, :name, :content, :content2, :description, :cta_button, :background, :thumbnail, :header, :body1
+  permit_params :user_id, :name, :tag, :content, :content2, :description, :cta_button, :background, :thumbnail, :header, :body1
 
   form html: {multipart: true} do |f|
     f.inputs "Article" do
       f.input :user_id, label: "User", as: :select, collection: User.all.map { |u| [u.full_name, u.id] }
       f.input :name
+      f.input :tag, as: :select, collection: ["design", "social", "events", "business", "web"]
       f.input :content
       f.input :content2
       f.input :description
